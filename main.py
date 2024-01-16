6d30ff789759a0cde00fc0fc314da09a5d6e0f58
robin <perperam@icatas.eu>
2024-01-16 16:33:58 +0100

    processor can now work with the case if there is no gps signal

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from shapely.geometry import Point
 from time import sleep
 from typing import Callable
+import logging
+
+from shapely.geometry import Point
 
 from Blacklist import Blacklist
-from Simulator import Simulator
+from Simulator import Simulator, Sensor
 
 
 class Handler:
+    # Add a Debouncer time stamp here
     is_blocked = False
 
     @staticmethod
@@ -27,35 +30,61 @@ class Processor:
         self.data_endpoint = data_endpoint
         self.blacklist_areas = blacklist.get_areas()
 
-    def coordinate_in_areas(self, gps_coordinate: Point) -> bool:
+        '''
+        setting_data_is_none
+        0: keep_current_state
+        1: unblock_everything
+        2: block_everything
+        '''
+        self.setting_no_data = 1
+
+    def coordinate_in_areas(self, gps_coordinate: Point) -> str | None:
         point = Point(gps_coordinate)
 
-        for area in self.blacklist_areas:
-            if point.within(area):
-                return True
+        for area_key in self.blacklist_areas.keys():
+            if point.within(self.blacklist_areas[area_key]):
+                print(f'inside the {area_key} area')
+                return area_key
             else:
-                return False
+                return None
 
     def run(self) -> None:
         while True:
-            if self.coordinate_in_areas(self.get_cord()):
-                Handler.block()
+            cord = self.get_cord()
+            print(cord)
+            if cord is None:
+                match self.setting_no_data:
+                    case 0:
+                        pass
+                    case 1:
+                        Handler.unblock()
+                    case 2:
+                        Handler.block()
             else:
-                Handler.unblock()
+                if self.coordinate_in_areas(cord):
+                    Handler.block()
+                else:
+                    Handler.unblock()
 
-    def get_cord(self) -> Point:
+    def get_cord(self) -> Point | None:
         sleep(2)
-        point = Point(self.data_endpoint())
-        print(point)
-        return point
+
+        data = self.data_endpoint()
+        if isinstance(data, tuple):
+            return Point(self.data_endpoint())
+        else:
+            return None
 
 
 # Example usage
 if __name__ == "__main__":
     s = Simulator(
-        [[1, 1], [1.5, 1.5], [2, 2], [2.5, 2.5], [3, 3], [3.5, 3.5], [4, 4], [4.5, 4.5], [5, 5], [5.5, 5.5]],
+        [[1.0, 1.0], [1.5, 1.5], [2.0, 2.0], [2.5, 2.5], [3.0, 3.0], [3.5, 3.5], [4.0, 4.0], [4.5, 4.5], None, [5.0, 5.0], [5.5, 5.5]],
         interval=5
     )
-    b = Blacklist("./blacklist.json")
-    p = Processor(s.get_geolocation, b)
+
+    g = Sensor()
+
+    blacklist = Blacklist("./blacklist.json")
+    p = Processor(s.get_geolocation, blacklist)
     p.run()
